d4114f30a5c64d2c4bc8a6a3d74efb10097c868a
citizen86 <108478848+citizen86@users.noreply.github.com>
2024-01-07 08:38:34 -0600

    010723.1

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,4 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const lightbox = document.createElement('div')
+lightbox id = 'lightbox'
+document.body.appendChild(lightbox)
