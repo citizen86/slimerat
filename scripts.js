239d7a1282c5578b4163296022e8315ccc5e5ccd
citizen86 <108478848+citizen86@users.noreply.github.com>
2024-08-24 12:07:18 -0500

    grid fixes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-const lightbox = document.createElement('div')
-lightbox id = 'lightbox'
-document.body.appendChild(lightbox)
+
+const lightboxBG = document.createElement('div');
+lightboxBG.id = "lightboxBG";
+document.body.appendChild(lightboxBG);
+
+const images = document.querySelectorAll('.image2, .image3, .image4, .image6, .image33, .image66');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightboxBG.classList.add('active');
+        const lightboxIMG = document.createElement('img');
+        lightboxIMG.src = image.src;
+        lightboxIMG.id = "lightboxIMG";
+        while (lightboxBG.firstChild){
+            lightboxBG.removeChild(lightboxBG.firstChild)
+        }
+        lightboxBG.appendChild(lightboxIMG);
+    })
+})
+
+lightboxBG.addEventListener('click', e => {
+    lightboxBG.classList.remove('active');
+})
